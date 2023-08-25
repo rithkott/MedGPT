aba534a23027f38535713010502022f4539f077a
Rithvik Kottapalli <rithkott@gmail.com>
2023-08-25 13:47:33 +0530

    completed medgpt

--- medgpt.py
+++ medgpt.py
@@ -2,13 +2,13 @@ from pymed import PubMed
 from pprint import pprint
 import json
 import openai
-
-def pubMedSearch(search):
+openai.api_key = ''
+def pubMedSearch(query):
     email = "myemail@gmail.com"
     pubmed = PubMed(tool="PubMedSearcher", email=email)
 
     #Carries out the Search through Pubmed API
-    results = pubmed.query(search, max_results=5)
+    results = pubmed.query(query, max_results=5)
     articleList = []
     articleInfo = []
 
@@ -30,34 +30,63 @@ def pubMedSearch(search):
                         u'results': article['results'],
                     })
     #print articleInfo to a json file for ease of viewing
-    with open('articleInfo.json', 'w') as articles_file:
-        json.dump(articleInfo, articles_file, indent = 4)
+    # with open('articleInfo.json', 'w') as articles_file:
+    #     json.dump(articleInfo, articles_file, indent = 4)
     
     return articleInfo
 
-def get_answer(question,knowledge):
-    openai.api_key = ''
+def get_query(question):
 
-    prompt = f"Answer the following question based on the knowledge I am providing. Don't answer from what you have been trained. If you can't answer from the knowledge given, tell that you don't have enough information to answer the same: {question} Knowledge: {knowledge} \nAnswer:"
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
+    query = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a physicians assistant."},
-            {"role": "user", "content": prompt}
+            {"role": "system", "content": "You are a research assistant that makes pubmed queries from user input."},
+            {"role": "user", "content": f"{question}?"},
+        ],
+        functions=[
+            {
+                "name": "pubMedSearch",
+                "description": "Get research documents from pubmed database matching our query",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "Query": {
+                            "type": "string",
+                            "description": "The query using keywords that will bring related information from pubmed database for the task asked"
+                        }
+                    },
+                    "required": ["Query"]
+                }
+            }
         ]
     )
 
-    return response.choices[0].message["content"].strip()
+    return query['choices'][0]['message']['function_call']
+
+def get_answer(question,knowledge):
+    prompt = f"Answer the following question based on the knowledge i am providing. Dont answer from what you have been trained. if you cant answer from the knowledge given tell that you dont have enough information to answer the same: {question} Knowledge: {knowledge} \nAnswer:"
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a research assistant that takes in data from pubmed articles and does what the user asks only using the knowledge the user provides to you. Do not use your own trained data."},
+            {"role": "user", "content": f"Question: {question}  Knowledge: {knowledge}"},
+        ],
+    )
+    
+    answer = response.choices[0] if response.choices else "No answer found"
+    return answer
+    
 
 def main():
     #Take user Input
     search = input("Enter your MedGPT Query: ")
     #Search for the articles in PubMed
-    articles = pubMedSearch(search)
-
     #Get a response from chatGPT
-    response = get_answer(search, articles)
-    print(response)
+    response = get_query(search)
+    print(response['arguments'])
+    data = pubMedSearch(response['arguments'])
+    answer = get_answer(search, data)
+    print(answer['message']['content'])
 
 main()
